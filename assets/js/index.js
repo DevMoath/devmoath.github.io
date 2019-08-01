b00f405e3c230cd217cb1cdff04cb6c9391d5013
Moath <mooath97@gmail.com>
2019-08-01 05:54:04 +0300

    Change js files location

---
+++ assets/js/index.js
@@ -0,0 +1,30 @@
+class UI {
+    constructor() {
+        this.page_loader = document.getElementById('page_loader');
+        this.body_page = document.getElementById('body_page');
+    }
+
+    start_type(name, major) {
+        new Typed('.name', {
+            strings: [name],
+            showCursor: true,
+            typeSpeed: 100
+        });
+
+        new Typed('.major', {
+            strings: [major],
+            showCursor: true,
+            typeSpeed: 130
+        });
+    }
+}
+
+window.onload = function() {
+    const ui = new UI();
+    ui.page_loader.classList.add('d-none');
+    ui.body_page.classList.remove('overflow-hidden');
+    let href = window.location.href;
+    href.includes('ar') || href.includes('AR')
+        ? ui.start_type('معاذ الهاجري', 'مطور')
+        : ui.start_type('Moath Alhajri', 'Developer');
+};
