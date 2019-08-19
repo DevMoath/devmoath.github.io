f845cc3be185a61eb4b6998daeea24c01af7be3d
Moath <mooath97@gmail.com>
2019-08-19 19:05:17 +0300

    Update the content

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,14 +3,12 @@ class UI {
         this.page_loader = document.getElementById('page_loader');
         this.body_page = document.getElementById('body_page');
     }
-
     start_type(name, major) {
         new Typed('.name', {
             strings: [name],
             showCursor: true,
             typeSpeed: 100
         });
-
         new Typed('.major', {
             strings: [major],
             showCursor: true,
@@ -18,13 +16,12 @@ class UI {
         });
     }
 }
-
 window.onload = function() {
     const ui = new UI();
     ui.page_loader.classList.add('d-none');
     ui.body_page.classList.remove('overflow-hidden');
     let href = window.location.href;
     href.includes('ar') || href.includes('AR')
-        ? ui.start_type('معاذ الهاجري', 'مطور')
-        : ui.start_type('Moath Alhajri', 'Developer');
+        ? ui.start_type('معاذ الهاجري', 'مطور ويب')
+        : ui.start_type('Moath Alhajri', 'Web Developer');
 };
