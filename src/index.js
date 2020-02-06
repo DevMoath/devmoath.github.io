892e5015d496d34c924f419a2e4e5bccdc6ca720
DevMoath <moath.alhajrii@gmail.com>
2020-02-07 01:43:25 +0300

    Add icon, update page font

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ require("bootstrap/dist/css/bootstrap.min.css");
 require("@fortawesome/fontawesome-free/js/all");
 require("./custom.css");
 window.onload = e => {
-    console.log("Test");
     document.querySelector("#year").innerText         = moment().format("YYYY");
     document.querySelector("#full_time").innerText    = moment("20191006", "YYYYMMDD").fromNow(true);
     document.querySelector("#current_time").innerText = moment("20200106", "YYYYMMDD").fromNow(true);
