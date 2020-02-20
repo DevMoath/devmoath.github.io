1ac8a74dee1d5277cf43499e7ea69324ddbbccb9
DevMoath <moath.alhajrii@gmail.com>
2020-02-21 00:39:41 +0300

    remove unused code, update Experience section

--- src/index.js
+++ src/index.js
@@ -4,25 +4,7 @@ require("bootstrap/dist/css/bootstrap.min.css");
 require("@fortawesome/fontawesome-free/js/all");
 require("./custom.css");
 window.onload = e => {
-    document.querySelector("#year").innerText         = moment().format("YYYY");
-    document.querySelector("#full_time").innerText    = moment("20191006", "YYYYMMDD").fromNow(true);
-    document.querySelector("#current_time").innerText = moment("20200106", "YYYYMMDD").fromNow(true);
-    $("a[href^='#']").on("click", function (e) {
-        let target = $(this.getAttribute("href"));
-        if (target.length) {
-            e.preventDefault();
-            $("html, body").stop().animate({
-                scrollTop: target.offset().top
-            }, 1000);
-        }
-    });
-    window.addEventListener("scroll", e => {
-        let link = $("#link");
-        if (window.scrollY < 30) {
-            link.fadeIn();
-        } else {
-            link.fadeOut();
-        }
-    });
+    document.querySelector("#year").innerText      = moment().format("YYYY");
+    document.querySelector("#full_time").innerText = document.querySelector("#current_time").innerText = moment("20191006", "YYYYMMDD").fromNow(true);
     document.addEventListener("gesturestart", e => e.preventDefault());
 };
