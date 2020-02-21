bb721b8167211f36cee09ba19d209434fdd82b45
DevMoath <moath.alhajrii@gmail.com>
2020-02-22 01:18:13 +0300

    update nodes packages

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-window.$      = require("jquery");
 window.moment = require("moment");
 require("bootstrap/dist/css/bootstrap.min.css");
-require("@fortawesome/fontawesome-free/js/all");
+require("@fortawesome/fontawesome-free/js/fontawesome.min");
+require("@fortawesome/fontawesome-free/js/brands.min");
 require("./custom.css");
 window.onload = e => {
     document.querySelector("#year").innerText      = moment().format("YYYY");
