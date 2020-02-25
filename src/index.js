7f61ad026ac3d75853db302ccd951c2d118a75b5
DevMoath <moath.alhajrii@gmail.com>
2020-02-26 01:14:46 +0300

    Add projects associated with the company I working at

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ require("@fortawesome/fontawesome-free/js/solid.min");
 require("./custom.css");
 window.onload = e => {
     document.querySelector("#year").innerText      = moment().format("YYYY");
-    document.querySelector("#full_time").innerText = document.querySelector("#current_time").innerText = moment("20191006", "YYYYMMDD").fromNow(true);
+    document.querySelector("#full_time").innerText = moment("20191006", "YYYYMMDD").fromNow(true);
     document.addEventListener("gesturestart", e => e.preventDefault());
     let base = "https://api.github.com/";
     fetch(`${base}users/devmoath/repos?sort=updated`)
