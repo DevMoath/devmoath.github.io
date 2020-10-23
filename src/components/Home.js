a34acd210c56bca20e7f3cc1b7db497a22ad38f3
Moath <moath.alhajrii@gmail.com>
2020-10-23 05:04:04 +0300

    show forked repo

--- src/components/Home.js
+++ src/components/Home.js
@@ -20,7 +20,7 @@ class Home extends Component {
 
     componentDidMount() {
         axios.get("https://api.github.com/users/devmoath/repos?sort=created").then(response => {
-            const projects = response.data.filter(item => !item.fork).slice(0, 5);
+            const projects = response.data.slice(0, 5);
             this.setState({projects: projects});
         }).catch(e => {
             this.setState({failed: true});
