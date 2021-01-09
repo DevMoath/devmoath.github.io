94c231edab51814986e6c9c620a4410924f29f28
Moath <moath.alhajrii@gmail.com>
2021-01-10 00:01:58 +0300

    move to vercel and update packages

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,5 @@
 import '../scss/moath.scss'
 
-function App({ Component, pageProps }) {
-    return <Component {...pageProps} />
-}
+const App = ({ Component, pageProps }) => <Component {...pageProps}/>
 
 export default App
