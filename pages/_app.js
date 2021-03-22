56ab84f9ad45e5516ab5bafcbff20f84944cd854
Moath <28797003+DevMoath@users.noreply.github.com>
2021-03-22 16:43:10 +0300

    install tailwind css

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/global.css';
+import 'tailwindcss/tailwind.css';
 
 const App = ({ Component, pageProps }) => <Component {...pageProps} />;
 
