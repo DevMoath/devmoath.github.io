77f297d51c01067cc513d9f8a9472d4371a62801
Moath <moath.alhajrii@gmail.com>
2021-04-30 12:12:48 +0300

    remove say hello button and add button to copy email

--- components/Preview.js
+++ components/Preview.js
@@ -1,6 +1,36 @@
 import Image from 'next/image';
+import { useEffect, useState } from 'react';
 
 const Preview = () => {
+    const [state, setState] = useState('idle');
+
+    useEffect(() => {
+        let current = true;
+
+        if (state === 'copying') {
+            navigator.clipboard
+                .writeText('moath.alhajrii@gmail.com')
+                .then(() => {
+                    if (current) {
+                        setState('copied');
+                    }
+                })
+                .catch(() => {
+                    if (current) {
+                        setState('error');
+                    }
+                });
+        } else if (state === 'copied' || state === 'error') {
+            window.setTimeout(() => {
+                if (current) {
+                    setState('idle');
+                }
+            }, 2000);
+        }
+
+        return () => (current = false);
+    }, [state]);
+
     return (
         <div className="mx-7 h-screen flex items-center">
             <div>
@@ -98,19 +128,17 @@ const Preview = () => {
                         </svg>
                     </a>
                 </div>
-                <div>
-                    <div className="flex justify-center">
-                        <a
-                            className="focus:outline-none text-white text-sm py-2.5 px-5 rounded-md bg-blue-500 hover:bg-blue-600 hover:shadow-lg"
-                            href="mailto:Moath.alhajrii@gmail.com?subject=Website%20Inquiry&body=Hello"
-                        >
-                            Say Hello
-                        </a>
-                    </div>
-                    <div className="shadow rounded-lg mt-4 mx-auto w-2/3 bg-white">
-                        <p className="px-4 py-3 text-center">Moath.Alhajrii@gmail.com</p>
-                    </div>
-                </div>
+                <button
+                    type="button"
+                    className="mx-auto shadow-lg w-full sm:w-auto flex-none bg-gray-50 text-gray-400 hover:text-gray-900 font-mono leading-6 py-3 sm:px-6 border border-gray-200 rounded-xl flex items-center justify-center space-x-2 sm:space-x-4 focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-gray-300 focus:outline-none transition-colors duration-200"
+                    onClick={() => setState('copying')}
+                >
+                    <span className="text-gray-900">moath.alhajrii@gmail.com</span>
+                    <span className="sr-only">(click to copy to clipboard)</span>
+                    <svg width="24" height="24" fill="none" stroke="currentColor" strokeWidth={1.5}>
+                        <path d="M8 16c0 1.886 0 2.828.586 3.414C9.172 20 10.114 20 12 20h4c1.886 0 2.828 0 3.414-.586C20 18.828 20 17.886 20 16v-4c0-1.886 0-2.828-.586-3.414C18.828 8 17.886 8 16 8m-8 8h4c1.886 0 2.828 0 3.414-.586C16 14.828 16 13.886 16 12V8m-8 8c-1.886 0-2.828 0-3.414-.586C4 14.828 4 13.886 4 12V8c0-1.886 0-2.828.586-3.414C5.172 4 6.114 4 8 4h4c1.886 0 2.828 0 3.414.586C16 5.172 16 6.114 16 8" />
+                    </svg>
+                </button>
             </div>
         </div>
     );
