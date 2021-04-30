78928535ec4afd1aeb681d9cffe442d920e85856
Moath <moath.alhajrii@gmail.com>
2021-05-01 00:39:47 +0300

    cleaning and change icon after copied email

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
+    mode: 'jit',
+
     purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+
     darkMode: false,
+
     theme: {
         extend: {
             fontFamily: {
@@ -10,8 +14,10 @@ module.exports = {
             },
         },
     },
+
     variants: {
         extend: {},
     },
+
     plugins: [],
 };
