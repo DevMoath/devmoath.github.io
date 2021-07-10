d04473f774548b7650db686a3a3fbe9637810144
Moath <moath.alhajrii@gmail.com>
2021-07-10 03:49:31 +0300

    fix code style

--- components/Preview.js
+++ components/Preview.js
@@ -137,12 +137,7 @@ const Preview = () => {
                         </svg>
                     ) : state === 'error' ? (
                         <svg width="24" height="24" fill="none" stroke="currentColor" strokeWidth={1.5}>
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth="2"
-                                d="M6 18L18 6M6 6l12 12"
-                            />
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
                         </svg>
                     ) : (
                         <svg width="24" height="24" fill="none" stroke="currentColor" strokeWidth={1.5}>
