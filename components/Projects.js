664a7844f6f3adf120a88b9eb6603d5cc56de012
Moath <moath.alhajrii@gmail.com>
2021-07-12 18:54:29 +0300

    use border instead of divider

--- components/Projects.js
+++ components/Projects.js
@@ -1,44 +1,47 @@
 import Image from 'next/image';
 import projects from '@data/projects';
 
+function classNames(...classes) {
+    return classes.filter(Boolean).join(' ');
+}
+
 export default function Projects() {
+    const length = projects.length - 1;
+
     return (
         <div className="py-16">
             <h1 className="mb-10 text-center text-4xl">Projects</h1>
-            {projects.map((project) => {
+            {projects.map((project, index) => {
                 return (
-                    <div className="my-7" key={Math.random()}>
-                        <div>
-                            <div className="flex gap-4">
-                                {(project.image && (
-                                    <Image
-                                        alt="project logo"
-                                        className="rounded-lg"
-                                        loading="lazy"
-                                        width="75"
-                                        height="75"
-                                        layout="fixed"
-                                        src={project.image}
-                                    />
-                                )) || (
-                                    <div className="w-[75px] h-[75px] min-w-[75px] min-h-[75px] bg-gray-600 rounded-lg flex">
-                                        <span className="m-auto">no logo</span>
-                                    </div>
-                                )}
-                                <h2 className="text-2xl my-auto">{project.title}</h2>
-                            </div>
-                            <blockquote className="mt-4 pl-4 py-3 border-l-4">
-                                <p>{project.description}</p>
-                            </blockquote>
-                            {project.link && (
-                                <div className="mt-5">
-                                    <a className="btn" href={project.link}>
-                                        Visit the project
-                                    </a>
+                    <div className={classNames('my-10 pb-10', index < length && 'border-b-2')} key={Math.random()}>
+                        <div className="flex gap-4">
+                            {(project.image && (
+                                <Image
+                                    alt="project logo"
+                                    className="rounded-lg"
+                                    loading="lazy"
+                                    width="75"
+                                    height="75"
+                                    layout="fixed"
+                                    src={project.image}
+                                />
+                            )) || (
+                                <div className="w-[75px] h-[75px] min-w-[75px] min-h-[75px] bg-gray-600 rounded-lg flex">
+                                    <span className="m-auto">no logo</span>
                                 </div>
                             )}
+                            <h2 className="text-2xl my-auto">{project.title}</h2>
                         </div>
-                        <div className="divider" />
+                        <blockquote className="mt-4 pl-4 py-3 border-l-4">
+                            <p>{project.description}</p>
+                        </blockquote>
+                        {project.link && (
+                            <div className="mt-5">
+                                <a className="btn" href={project.link}>
+                                    Visit the project
+                                </a>
+                            </div>
+                        )}
                     </div>
                 );
             })}
