617aae7c461dbbf043485216d3d76bc0e020003b
Moath <moath.alhajrii@gmail.com>
2021-07-18 03:54:27 +0300

    add redirects

--- next.config.js
+++ next.config.js
@@ -39,4 +39,28 @@ module.exports = {
             },
         ];
     },
+    async redirects() {
+        return [
+            {
+                source: '/twitter',
+                destination: 'https://twitter.com/Dev_Moath',
+                permanent: true,
+            },
+            {
+                source: '/github',
+                destination: 'https://github.com/DevMoath',
+                permanent: true,
+            },
+            {
+                source: '/linkedin',
+                destination: 'https://www.linkedin.com/in/moath-alhajri/',
+                permanent: true,
+            },
+            {
+                source: '/youtube',
+                destination: 'https://www.youtube.com/channel/UCm-qdl6K8w6hvxFwgO7u5qw',
+                permanent: true,
+            },
+        ];
+    },
 };
