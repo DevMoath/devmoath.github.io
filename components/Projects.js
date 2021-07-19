9a926b7e2d7edac82a652534b77e86339e398fce
Moath <moath.alhajrii@gmail.com>
2021-07-19 19:41:35 +0300

    update Projects style

--- components/Projects.js
+++ components/Projects.js
@@ -1,46 +1,41 @@
-import Image from 'next/image';
 import projects from '@data/projects';
-import classNames from 'classnames';
 
 export default function Projects() {
-    const length = projects.length - 1;
-
     return (
-        <>
-            {projects.map((project, index) => {
+        <div className="flex flex-col gap-8 my-10">
+            {projects.map((project) => {
                 return (
-                    <div className={classNames('mt-10 pb-10', index < length && 'border-b-2 mb-10')} key={Math.random()}>
-                        <div className="flex gap-4">
-                            {(project.image && (
-                                <Image
-                                    alt="project logo"
-                                    className="rounded-lg"
-                                    loading="lazy"
-                                    width="75"
-                                    height="75"
-                                    layout="fixed"
-                                    src={project.image}
-                                />
-                            )) || (
-                                <div className="w-[75px] h-[75px] min-w-[75px] min-h-[75px] bg-gray-600 rounded-lg flex">
-                                    <span className="m-auto">no logo</span>
+                    <div className="card bordered shadow-lg" key={Math.random()}>
+                        <div className="card-body">
+                            <div className="card-title">
+                                <div className="flex gap-4 mb-5">
+                                    {(project.image && (
+                                        <img
+                                            alt={`${project.title} logo`}
+                                            className="rounded-lg w-[60px] h-[60px] min-h-[60px] min-w-[60px] lg:w-[80px] lg:h-[80px] lg:min-w-[80px] lg:min-h-[80px]"
+                                            loading="lazy"
+                                            src={project.image}
+                                        />
+                                    )) || (
+                                        <div className="w-[60px] h-[60px] min-h-[60px] min-w-[60px] lg:w-[80px] lg:h-[80px] lg:min-w-[80px] lg:min-h-[80px] bg-gray-600 rounded-lg flex">
+                                            <span className="m-auto text-xs lg:text-lg">no logo</span>
+                                        </div>
+                                    )}
+                                    <h2 className="text-lg lg:text-2xl my-auto">{project.title}</h2>
+                                </div>
+                            </div>
+                            <p>{project.description}</p>
+                            {project.link && (
+                                <div className="justify-end card-actions">
+                                    <a className="btn" href={project.link}>
+                                        Visit the project
+                                    </a>
                                 </div>
                             )}
-                            <h2 className="text-2xl my-auto">{project.title}</h2>
                         </div>
-                        <blockquote className="mt-4 pl-4 py-3 border-l-4">
-                            <p>{project.description}</p>
-                        </blockquote>
-                        {project.link && (
-                            <div className="mt-5">
-                                <a className="btn" href={project.link}>
-                                    Visit the project
-                                </a>
-                            </div>
-                        )}
                     </div>
                 );
             })}
-        </>
+        </div>
     );
 }
