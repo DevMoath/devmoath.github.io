7388517837b7dfa2659581eb015e984cd0cd1ece
Moath <moath.alhajrii@gmail.com>
2021-07-19 18:25:04 +0300

    install @heroicons/react

--- components/Preview.js
+++ components/Preview.js
@@ -2,6 +2,7 @@ import Image from 'next/image';
 import { useState } from 'react';
 import copy from '@devmoath/copy-text';
 import Link from 'next/link';
+import { ClipboardCheckIcon, ClipboardIcon } from '@heroicons/react/outline';
 
 const svgProps = {
     viewBox: '0 0 24 24',
@@ -108,15 +109,7 @@ const Preview = () => {
                 <div className="flex">
                     <button type="button" className="btn mx-auto shadow-lg" onClick={() => copyEmail()}>
                         <span className="mr-3">{email}</span>
-                        {state ? (
-                            <svg width="24" height="24" fill="none" stroke="currentColor" strokeWidth={1.5}>
-                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" />
-                            </svg>
-                        ) : (
-                            <svg width="24" height="24" fill="none" stroke="currentColor" strokeWidth={1.5}>
-                                <path d="M8 16c0 1.886 0 2.828.586 3.414C9.172 20 10.114 20 12 20h4c1.886 0 2.828 0 3.414-.586C20 18.828 20 17.886 20 16v-4c0-1.886 0-2.828-.586-3.414C18.828 8 17.886 8 16 8m-8 8h4c1.886 0 2.828 0 3.414-.586C16 14.828 16 13.886 16 12V8m-8 8c-1.886 0-2.828 0-3.414-.586C4 14.828 4 13.886 4 12V8c0-1.886 0-2.828.586-3.414C5.172 4 6.114 4 8 4h4c1.886 0 2.828 0 3.414.586C16 5.172 16 6.114 16 8" />
-                            </svg>
-                        )}
+                        {state ? <ClipboardCheckIcon className="h-6 w-6" /> : <ClipboardIcon className="h-6 w-6" />}
                     </button>
                 </div>
             </div>
