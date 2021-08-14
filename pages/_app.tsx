3505fdbd3071e70b01c97783c72b3df1db9f3cec
Moath <moath.alhajrii@gmail.com>
2021-08-14 21:06:45 +0300

    remove seo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,6 @@
 import 'tailwindcss/tailwind.css';
-import { Fragment } from 'react';
-import SEO from '@components/SEO';
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
-    return (
-        <Fragment>
-            <SEO />
-            <Component {...pageProps} />
-        </Fragment>
-    );
+    return <Component {...pageProps} />;
 }
