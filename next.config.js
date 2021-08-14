eb5d852b06c9d754f952027586be69c9eea4a443
Moath <moath.alhajrii@gmail.com>
2021-08-14 21:04:33 +0300

    add headers

--- next.config.js
+++ next.config.js
@@ -1,15 +1,54 @@
+const securityHeaders = [
+    {
+        key: 'X-DNS-Prefetch-Control',
+        value: 'on',
+    },
+    {
+        key: 'Strict-Transport-Security',
+        value: 'max-age=31536000; includeSubDomains; preload',
+    },
+    {
+        key: 'X-XSS-Protection',
+        value: '1; mode=block',
+    },
+    {
+        key: 'X-Frame-Options',
+        value: 'SAMEORIGIN',
+    },
+    {
+        key: 'Permissions-Policy',
+        value: 'camera=(), microphone=(), geolocation=(), interest-cohort=()',
+    },
+    {
+        key: 'X-Content-Type-Options',
+        value: 'nosniff',
+    },
+    {
+        key: 'Referrer-Policy',
+        value: 'origin-when-cross-origin',
+    },
+];
+
 module.exports = {
     reactStrictMode: true,
+    async headers() {
+        return [
+            {
+                source: '/(.*)',
+                headers: securityHeaders,
+            },
+        ];
+    },
     async redirects() {
         return [
             {
                 source: '/twitter',
-                destination: 'https://twitter.com/Dev_Moath',
+                destination: 'https://twitter.com/intent/follow?screen_name=dev_moath',
                 permanent: true,
             },
             {
                 source: '/github',
-                destination: 'https://github.com/DevMoath',
+                destination: 'https://github.com/devmoath',
                 permanent: true,
             },
             {
