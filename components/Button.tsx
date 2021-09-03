0f6d4b032e71fb69de9044ee8167a221de72b685
Moath <moath.alhajrii@gmail.com>
2021-09-03 16:16:57 +0300

    design design design ...

--- components/Button.tsx
+++ components/Button.tsx
@@ -3,13 +3,9 @@ import classnames from 'classnames';
 
 export default function Button({ children, href, classNames, asLink }: { children: any; href: string; classNames?: string; asLink?: boolean }) {
     classNames = classnames(
-        'flex justify-center text-center transition-colors duration-300 transform px-3 py-2 rounded-lg text-black hover:text-white hover:bg-blue-500',
+        'flex justify-center text-center transition-colors duration-300 transform px-3 py-2 rounded-lg text-black hover:text-white hover:bg-gray-900',
         classNames
     );
 
-    return (
-        <Link href={href} passHref>
-            {asLink ? <a className={classNames}>{children}</a> : <button className={classNames}>{children}</button>}
-        </Link>
-    );
+    return <Link href={href}>{asLink ? <a className={classNames}>{children}</a> : <button className={classNames}>{children}</button>}</Link>;
 }
