ef4e593e615497f9fa2b191ba9a65e0c2f751d6f
Moath <moath.alhajrii@gmail.com>
2021-09-03 15:50:20 +0300

    add about me

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,48 +1,35 @@
-import Preview from '@components/Preview';
-import Projects from '@components/Projects';
-import { useState } from 'react';
-import Snippets from '@components/Snippets';
-import classNames from 'classnames';
-import { BriefcaseIcon, CodeIcon } from '@heroicons/react/outline';
+import { Fragment } from 'react';
 import SEO from '@components/SEO';
 import NavBar from '@components/NavBar';
+import Image from 'next/image';
 
 export default function Index() {
-    const [tab, setTab] = useState('projects');
-
     return (
-        <div>
+        <Fragment>
             <SEO />
             <NavBar />
-            <div className="lg:flex">
-                <div className="h-screen lg:py-0 max-w-xl">
-                    <Preview />
-                </div>
-                <div className="px-7 text-white bg-gray-700 lg:h-screen lg:overflow-auto lg:w-full pt-10" data-theme="dark">
-                    <div className="tabs tabs-boxed bg-neutral" data-theme="corporate">
-                        <a
-                            className={classNames('tab w-1/2 tab-lg transition-all duration-200 !text-white', tab === 'projects' && 'tab-active')}
-                            onClick={() => setTab('projects')}
-                        >
-                            <span className="hidden lg:block mr-1">My</span>
-                            Projects <BriefcaseIcon className="w-6 h-6 ml-1" />
-                        </a>
-                        <a
-                            className={classNames('tab w-1/2 tab-lg transition-all duration-200 !text-white', tab === 'snippets' && 'tab-active')}
-                            onClick={() => setTab('snippets')}
-                        >
-                            <span className="hidden lg:block mr-1">My Code</span>
-                            Snippets <CodeIcon className="w-6 h-6 ml-1" />
-                        </a>
-                    </div>
-                    <div className={tab !== 'projects' ? 'hidden' : ''}>
-                        <Projects />
-                    </div>
-                    <div className={tab !== 'snippets' ? 'hidden' : ''}>
-                        <Snippets />
-                    </div>
-                </div>
+            <div className="container px-4 mt-20 grid grid-cols-3 gap-4">
+                <h1 className="text-4xl text-black font-bold col-span-4">Hello, I'm Moath</h1>
+                <p className="my-8 text-lg leading-8 col-span-4 md:col-span-2">
+                    I am Moath Alhajri from Saudi Arabia, I have bachelor degree in Information Systems from{' '}
+                    <a href="https://ksu.edu.sa/" className="text-blue-500 hover:underline">
+                        King Saud University
+                    </a>{' '}
+                    and I am working now as Software Developer at{' '}
+                    <a href="https://tamkeentech.sa/" className="text-blue-500 hover:underline">
+                        TamkeenTech
+                    </a>{' '}
+                    company.
+                </p>
+                <Image
+                    src="/image/code_logo.svg"
+                    width="200"
+                    height="200"
+                    layout="fixed"
+                    className="rounded-3xl col-span-4 md:col-span-2"
+                    alt="my logo"
+                />
             </div>
-        </div>
+        </Fragment>
     );
 }
