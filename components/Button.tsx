52e652e6d9ce15aef051cccea09af014deef2cca
Moath <moath.alhajrii@gmail.com>
2021-09-03 15:13:19 +0300

    add classNames

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,8 +1,11 @@
 import Link from 'next/link';
+import classnames from 'classnames';
 
-export default function Button({ children, href, asLink = false }) {
-    const classNames =
-        'flex justify-center text-center transition-colors duration-200 transform px-3 py-2 rounded-lg text-lg text-black hover:bg-gray-300';
+export default function Button({ children, href, classNames = '', asLink = false }) {
+    classNames = classnames(
+        'flex justify-center text-center transition-colors duration-200 transform px-3 py-2 rounded-lg text-black hover:bg-gray-300',
+        classNames
+    );
 
     return (
         <Link href={href} passHref>
